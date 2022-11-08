7ad81e04ffc41cd17266d97626805f40c4e3bf73
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-08 22:02:23 +0530

    Job component was created.

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -258,6 +258,14 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const setEditJob = (id) => {
+    console.log(`set edit job : ${id}`);
+  };
+
+  const deleteJob = (id) => {
+    console.log(`delete job : ${id}`);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -271,6 +279,8 @@ const AppProvider = ({ children }) => {
         clearValues,
         createJob,
         getJobs,
+        setEditJob,
+        deleteJob,
       }}
       //   value={{ ...state, displayAlert, registerUser, loginUser, setupUser }}
     >
